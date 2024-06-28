64607ce7e6188f818a9a49185746ab9b7865a9cd
Jack Calcinius <163590820+travyyx@users.noreply.github.com>
2024-06-28 03:55:08 +0000

    "Added interfaces for Person and Traits, implemented useState and useEffect to fetch data from API and display person's name"

--- whoami-app/src/pages/traits.tsx
+++ whoami-app/src/pages/traits.tsx
@@ -1,14 +1,51 @@
 import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 type Params = {
     name: string
 }
+
+export interface Person {
+    name:   string;
+    traits: Traits;
+}
+
+export interface Traits {
+    confidence: number;
+    smile:      number;
+    cuteness:   number;
+    love:       number;
+    kindness:   number;
+    anger:      number;
+}
+
+
+
 function TraitsPage() {
+    const [person, setPerson] = useState<Person>()
     const params = useParams<Params>()
     const navigate = useNavigate()
+    
+    const fetchData = async() => {
+        try {
+            const url = `https://whoami-backend.vercel.app/traits/${params.name?.replace(" ", "%20")}`;
+        
+            const response = await fetch(url);
+            const text = await response.text()
+            console.log(JSON.parse(text))
+            setPerson(JSON.parse(text))
+        } catch {
+            alert("An error occurred. Please try again.")
+        }
+
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     return (
         <main className="h-screen w-full flex flex-col justify-center bg-black items-center text-white">
-            {params.name}
+            {person && person.name}
         </main>
     )
 }
