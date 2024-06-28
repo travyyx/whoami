944e137d19c635a5ac096af4271b12f60381e828
Jack Calcinius <163590820+travyyx@users.noreply.github.com>
2024-06-28 03:51:59 +0000

    added cors to backend

--- whoami-backend/index.js
+++ whoami-backend/index.js
@@ -1,6 +1,9 @@
-import express from "express";
+var express = require('express')
+var cors = require('cors')
 const app = express();
 const port = 3000;
+
+app.use(cors())
 
 
 // Function to generate personality traits with weights
